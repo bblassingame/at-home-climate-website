7c0f5bd87842e22ecccd7c1e356ddfec0c369720
Bo Blassingame <bo.blassingame@gmail.com>
2017-08-31 20:43:02 -0500

    Updated the logo to link back to the Features info panel

--- src/logo.js
+++ src/logo.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoMain from './logo-main.jpg';
 
 const Logo = function() {
     return(
         <div className="logo-content">
-            <img src={logoMain} />
+            <Link to='/'>
+                <img src={logoMain} alt='The logo for At Home Climate Storage in Longview, TX'/>
+            </Link>
         </div>        
     )
 }
