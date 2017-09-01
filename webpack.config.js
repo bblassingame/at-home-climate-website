329aa8561e49996cd54f5fb3a751896431d9e00d
Bo Blassingame <bo.blassingame@gmail.com>
2017-08-31 19:31:44 -0500

    Added html-webpack-plugin to begin generation our index.html - Removed the index.html file we were previously using - Added the html-webpack-plugin and configured it - added template.ejs as the template for the html we generate

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ var BUILD_DIR = path.resolve(__dirname, 'bin/');
 var APP_DIR = path.resolve(__dirname, 'src/');
 var ENTRY_PATH = path.resolve(APP_DIR, 'main.js');
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: ['babel-polyfill', ENTRY_PATH],
     output: {
@@ -45,4 +47,10 @@ module.exports = {
     devServer: {
 
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'This is the future website for At Home Climate Control Storage in Longview, Texas',
+            template: './src/template.ejs'
+        })
+    ],
 };
