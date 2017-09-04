98cca41e1e88575c0c3dc14d395c19f8fcf8e37d
Bo Blassingame <bo.blassingame@gmail.com>
2017-09-04 17:31:15 -0500

    Updated the Contacts panel to have better information for contacting and leasing a unit. - Add some text to the Contacts component for address and phone number - Updated the styling for the Contacts component - Reordered the .css imports so that style.css is applied above the reset.css

--- src/contact.js
+++ src/contact.js
@@ -3,9 +3,21 @@ import React from 'react'
 const Contact = function() {
     return(
         <div className="contacts-content">
-            <div className="phone-container"><span className="phone">903-757-5845</span></div>
-            <div><span className="address">2000 Jahan Trail</span></div>
-            <div><span className="location">Located near McWhorter Park</span></div>
+            <div className="center-contacts-content">
+                <div className="phone-container">
+                    <p ><strong>Call today</strong> to reserve a unit...</p>
+                    <p className="phone">903-757-5845</p>
+                </div>
+                <div>
+                    
+                    <p className="address">2000 Jahan Trail  Longview, TX  75604</p>
+                    {/* <p className="address">2000 Jahan Trail</p>
+                    <p className="address">Longview, TX  75604</p> */}
+                </div>
+                <div>
+                    <p className="location">Located near McWhorter Park</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './reset.css';
 import './style.css';
+import './reset.css';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Starting Application');
