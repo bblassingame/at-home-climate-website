f17d0c712cfea9eabdff82676cfef0ec63985a45
Bo Blassingame <bo.blassingame@gmail.com>
2017-09-05 20:34:09 -0500

    Updated the telephone number to be clickable - Added some meta information to the template and panel for the phone number - Updated the styling so that the phone number didn't look like normal anchor text - Updated the import order of the two style sheets so that style.css is the last sheet to apply styles

--- src/contact.js
+++ src/contact.js
@@ -6,10 +6,13 @@ const Contact = function() {
             <div className="center-contacts-content">
                 <div className="phone-container">
                     <p className="call-today"><strong>Call today</strong> to reserve a unit...</p>
-                    <p className="phone">903-757-5845</p>
+                    <p className="phone">
+                        <span itemprop="telephone">
+                            <a href="tel:+19037575845" rel="nofollow">903-757-5845</a>
+                        </span>
+                    </p>
                 </div>
                 <div>
-                    
                     <p className="address">2000 Jahan Trail, Longview TX  75604</p>
                 </div>
                 <div>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './style.css';
 import './reset.css';
+import './style.css';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Starting Application');
