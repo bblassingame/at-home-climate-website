b58b769f002246cf249e89aa54db5cb222adda17
Bo Blassingame <bo.blassingame@gmail.com>
2017-09-16 16:03:29 -0500

    Added google analytics support - Added the react-ga package for integrating google analytics with react router - Updated the router components to log a pageview when they're clicked

--- src/app-content.js
+++ src/app-content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import ReactGA from 'react-ga'
 
 import Logo from './logo'
 import FeaturesAndNav from './features-nav'
@@ -7,13 +8,25 @@ import SizesAndRates from './sizes-rates'
 import MapAndLocation from './map-location'
 import Contact from './contact'
 
+ReactGA.initialize('UA-106318026-1',
+  {
+    debug: false,
+  }
+)
+
 const AppContent = function() {
+
+  const logPageView = function() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   return(
     <div className="app-content">
       <Logo />
-      <Route exact path='/' component={FeaturesAndNav} />
-      <Route path='/Rates' component={SizesAndRates} />
-      <Route path='/Location' component={MapAndLocation} />
+      <Route exact path='/' onUpdate={logPageView} component={FeaturesAndNav} />
+      <Route path='/Rates'onUpdate={logPageView}  component={SizesAndRates} />
+      <Route path='/Location' onUpdate={logPageView} component={MapAndLocation} />
       <Contact />
     </div>
   )
