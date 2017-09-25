ea6e3e2909acc20816447c1ec1a2bbaf1d2107c7
Bo Blassingame <bo.blassingame@gmail.com>
2017-09-24 19:04:51 -0500

    Added the content to the footer

--- src/layout-v2/footer.js
+++ src/layout-v2/footer.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return(
     <div className='footer'>
+      <div>
+        <p className='maps-link'>
+          <Link to='/Location'>Find us on Google Maps...</Link>
+        </p>
+      </div>
+      <div>
+        <p className="address">2000 Jahan Trail, Longview TX  75604</p>
+      </div>
+      <div>
+        <p className="location">Located near McWhorter Park</p>
+      </div>
     </div>
   )
 }
