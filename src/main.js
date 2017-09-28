2b6aee14be5db2254158c28955d0dede26ff25f6
Bo Blassingame <bo.blassingame@gmail.com>
2017-09-27 20:30:37 -0500

    Updated the debug statement to be specific that we're in a dev mode

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './layout-v2/style2.css'
 
 /*global process*/
 if (process.env.NODE_ENV !== 'production') {
-  console.log('Starting Application')
+  console.log('Dev Environment Detected:  Starting Application')
 }
 
 ReactDOM.render(
