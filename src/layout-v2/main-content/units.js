a3678ab91bc8309ccc7077104db96758da08de30
Bo Blassingame <bo.blassingame@gmail.com>
2017-10-03 21:49:29 -0500

    Filled in the Units panel with a template for what the units should look like - Added the rest of the basic styling for the details in the panel - Added the content for one row

--- src/layout-v2/main-content/units.js
+++ src/layout-v2/main-content/units.js
@@ -8,8 +8,20 @@ const Units = () => {
       <img className='mc-sub-panel-img' src={StorageIcon} alt='storage unit icon' />
       <div className='mc-sub-panel-header-container'>
         <h1 className='mc-sub-panel-header'>Storage Units</h1>
-        <p>multi-line storage</p>
-        <p>unit content go here</p>
+        <div>
+          <div>
+            <p className='units-dim-row'> {/*padding: 0 0 2px*/}
+              <span className='units-dimensions'>7.5' x 10'</span> {/*display: inline-block; width 8em; */}
+              <span>$75.00</span> {/**/}
+            </p>
+          </div>
+          <div>
+            <p className='units-info-container'> {/*font-size: .75em*/}
+              <span className='units-info-availability'>Available</span> {/*display: inline-block; width 8em; font-style: italic*/}
+              <span>7th month free & no administrative fee with 6 month lease and online reservation</span> {/**/}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
