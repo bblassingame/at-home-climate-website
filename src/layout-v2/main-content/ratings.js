dfb4b794f1d310c83733596402be413e44e3a459
Bo Blassingame <bo.blassingame@gmail.com>
2017-10-09 23:52:04 -0500

    Updated the Ratings panel - Added the ratings icons for the 1-5 star ratings - Added the main ratings icon for the header row - Added the initial rating - Added the rating data - Updated the styling needed for the ratings panel - Added the actual gold start icon

--- src/layout-v2/main-content/ratings.js
+++ src/layout-v2/main-content/ratings.js
@@ -1,31 +1,58 @@
 import React from 'react'
-// import ReactGA from 'react-ga'
 
-// import YelpLogo from './Yelp_Launch.png'
-// import SparefootLogo from './Sparefoot_Logo.jpg'
-// import FBLogo from './FB-f-Logo__blue_50.png'
+import RatingsIcon from './ratings-icon.png'
+import RatingsIconGold from './ratings-icon-gold.png'
 
 const Ratings = () => {
   return (
-    <div>
-      <h1>See What Others Say</h1>
-      {/* <div>
-        <ReactGA.OutboundLink eventLabel='Click To Yelp' to='https://www.yelp.com/biz/at-home-climate-storage-longview?osq=Climate+Controlled+Storage'>
-          <img className='img-fill' src={YelpLogo} alt='' />
-        </ReactGA.OutboundLink>
+    <div className='mc-sub-panel'>
+      <img className='mc-sub-panel-img' src={RatingsIcon} alt='a star image representing a section that displays user reviews' />
+      <div className='mc-sub-panel-header-container'>
+        <h1 className='mc-sub-panel-header'>Ratings</h1>
+        <div>
+          <div className='ratings-name-row'>
+            <p>K Thornton</p>
+            <div className='ratings-star-container'>
+              <img className='img-fill' src={RatingsIconGold} alt='a star image representing 1 of 5 total stars a reviewer can give on a review' />
+              <img className='img-fill' src={RatingsIconGold} alt='a star image representing 2 of 5 total stars a reviewer can give on a review' />
+              <img className='img-fill' src={RatingsIconGold} alt='a star image representing 3 of 5 total stars a reviewer can give on a review' />
+              <img className='img-fill' src={RatingsIconGold} alt='a star image representing 4 of 5 total stars a reviewer can give on a review' />
+              <img className='img-fill' src={RatingsIconGold} alt='a star image representing 5 of 5 total stars a reviewer can give on a review' />
+            </div>
+          </div>
+          <p className='ratings-title'>Great Value and Awesome Customer Service</p>
+          <p className='ratings-comment'>They have multi size units and will work with you if you lease over two units. They also offer electronic transfer for the monthly fee. Good customer service if any issue should arise.</p>
+        </div>
       </div>
-      <div>
-        <ReactGA.OutboundLink eventLabel='Click To Sparefoot' to='https://www.sparefoot.com/Longview-TX-self-storage/At-Home-Climate-Storage-153722.html'>
-          <img className='img-fill' src={SparefootLogo} alt='' />
-        </ReactGA.OutboundLink>
-      </div>
-      <div>
-        <ReactGA.OutboundLink eventLabel='Click To Facebook' to='https://www.facebook.com/AtHomeClimateStorage/'>
-          <img className='img-fill' src={FBLogo} alt='Visit At Home Climate Storage on Facebook' />
-        </ReactGA.OutboundLink>
-      </div> */}
     </div>
   )
 }
+
+const ratingsData = [
+  {
+    ID: 1,
+    reviewerName: 'K Thornton', 
+    rating: 5, 
+    title: 'Great Value and Awesome Customer Service', 
+    comment: 'They have multi size units and will work with you if you lease over two units. They also offer electronic transfer for the monthly fee. Good customer service if any issue should arise.', 
+    date: '20140311'
+  },
+  {
+    ID: 2,
+    reviewerName: 'Debbie Helbig', 
+    rating: 5, 
+    title: 'Good price for large storage area', 
+    comment: 'We rented a 7.5\' x 10\' unit which was quite ample for the amount of stuff that we had to store. The price per month was excellent for a climate controlled facility. Security at the facility seemed very good. The owners were very friendly and very helpful.', 
+    date: '20170507'
+  },
+  {
+    ID: 3,
+    reviewerName: 'Melissa A. Harris', 
+    rating: 5, 
+    title: '', 
+    comment: 'Best place for cleanliness and climate control.  Ashly Lloyd so easy to work with on maintaining our rental contracts and payments.  We rented for years!', 
+    date: '20170721'
+  },
+]
 
 export default Ratings
